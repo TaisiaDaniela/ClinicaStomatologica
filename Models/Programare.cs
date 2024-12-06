7f2bc2133c45df008f19537344bdb63acd0aaeb8
STEFÁNIA PATKO <stefania.patko@stud.ubbcluj.ro>
2024-12-06 10:07:33 +0200

    stefi

--- Models/Programare.cs
+++ Models/Programare.cs
@@ -22,37 +22,4 @@ namespace ClinicaStomatologica.Models
 
     }
 
-namespace ClinicaStomatologica.CustomValidations
-    {
-        public class ValidareDataProgramareAttribute : ValidationAttribute
-        {
-            public override bool IsValid(object? value)
-            {
-                if (value is DateTime dataProgramare)
-                {
-                    // Verifică dacă este sâmbătă sau duminică
-                    if (dataProgramare.DayOfWeek == DayOfWeek.Saturday || dataProgramare.DayOfWeek == DayOfWeek.Sunday)
-                    {
-                        return false; // Invalid dacă este weekend
-                    }
-
-                    // Verifică dacă ora este fixă (minute și secunde 0)
-                    if (dataProgramare.Minute != 0 || dataProgramare.Second != 0)
-                    {
-                        return false; // Invalid dacă nu este la o oră fixă
-                    }
-
-                    return true; // Valid dacă trece ambele condiții
-                }
-
-                return false; // Invalid dacă valoarea nu este un DateTime
-            }
-
-            public override string FormatErrorMessage(string name)
-            {
-                return $"{name} trebuie să fie la o oră fixă în timpul săptămânii (luni-vineri, ex. 10:00, 14:00, etc.).";
-            }
-        }
-    }
-
 }
